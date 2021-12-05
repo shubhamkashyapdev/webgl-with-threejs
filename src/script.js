e0e55473788f4b07fa6963a1fa557a40bb426c4d
Shubham Kashyap <hex.media.1800@gmail.com>
2021-12-05 15:04:25 +0530

    mouse track animation added to cube

--- src/script.js
+++ src/script.js
@@ -5,6 +5,24 @@ import gsap from 'gsap'
 
 // element
 const canvas = document.getElementById('webgl')
+// sizes
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+}
+window.addEventListener('mousemove', (e) => {
+  cursor.x = e.clientX / width - 0.5
+  cursor.y = -(e.clientY / height - 0.5)
+  console.log(cursor)
+})
 
 // new schene
 const scene = new THREE.Scene()
@@ -18,21 +36,16 @@ const mesh = new THREE.Mesh(geometry, material) // create a mesh with geometry &
 
 scene.add(mesh)
 
-// sizes
-const sizes = {
-  width: 800,
-  height: 600,
-}
 const { width, height } = sizes
 
 // camera
 
 // Perspective Camera: 1. field of view (degree | vertical) 2. Aspect Ratio 3.
-// const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100)
 
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -51,12 +64,18 @@ renderer.setSize(width, height)
 
 // capture the time
 const clock = new THREE.Clock()
+
 // animation
 const tick = () => {
   // Same time for animation
   const elapsedTime = clock.getElapsedTime()
 
-  mesh.rotation.y = elapsedTime
+  // mesh.rotation.y = elapsedTime
+
+  // update the camera
+  camera.position.x = cursor.x * 10
+  camera.position.y = cursor.y * 10
+  camera.lookAt(mesh.position)
 
   // render the screen
   renderer.render(scene, camera)
